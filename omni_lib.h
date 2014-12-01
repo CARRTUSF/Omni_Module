ae5bd90b1a52a1074ec448808e63694ec504bbcb
DanielAshley <dashley2@mail.usf.edu>
2014-12-01 14:14:08 -0500

    Updated functions and Removed Extra files

--- omni_lib.h
+++ omni_lib.h
@@ -19,6 +19,12 @@ namespace WMRA{
 		bool checkButton1();
 		bool checkButton2();
 		int isInitialized();
+		bool forceToOrigin();
+		bool setForceFeedback(double X, double Y, double Z);
+		bool setForceFeedback(double forceMag);
+		std::vector<double> getTorqueData();
+		std::vector<double> getPositionData();
+		std::vector<double> getVelocityData();
 	private:
 		HHD hHD;
 		int initialized;
