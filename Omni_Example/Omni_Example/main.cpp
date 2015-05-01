336a6112b12d62dfd4ec373cd1093623ca2bed2c
DanielAshley <dashley2@mail.usf.edu>
2015-05-01 16:50:39 -0400

    modified test program

--- Omni_Example/Omni_Example/main.cpp
+++ Omni_Example/Omni_Example/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 #include "omni_lib.h"
 
 using namespace std;
@@ -22,7 +23,7 @@ int main()
 	while(!o.checkButton1()) {}
 	cout << "\t...Button 1 pressed\n" << endl;
 
-
+/*
 	// Button 2 check
 	cout << "Waiting for button 2 to be pressed..." << endl;
 	while(!o.checkButton2()) {}
@@ -52,6 +53,16 @@ int main()
 		cout << "What force magnitude would you like to use? (Default: 0.1)" <<  endl;
 		cin >> mag;
 		o.setForceFeedback(mag);
+	}
+*/
+	o.forceToOrigin();
+	vector<double> p;
+	int count = 0;
+	while(count < 2000)
+	{
+		count++;
+		p = o.getPositionData();
+		std::cout << "Omni Position: [ " << p[0] << ", " << p[1] << ", " << p[2] << " ]" << std::endl;
 	}
 	return 1;
 }
